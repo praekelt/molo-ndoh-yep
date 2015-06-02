ca936d449edbb5d14a66d837f55c3a48991a8e2e
Milton Madanda <madandat@gmail.com>
2015-06-02 18:37:56 +0200

    ensure correct settings in prod

--- ndohyep/sideloader/postinstall.sh
+++ ndohyep/sideloader/postinstall.sh
@@ -1,6 +1,6 @@
 cd "${INSTALLDIR}/${NAME}/ndohyep/"
 manage="${VENV}/bin/python ${INSTALLDIR}/${NAME}/ndohyep/manage.py"
 
-$manage migrate
-$manage compress
-$manage collectstatic --noinput
+$manage migrate --settings=ndohyep.settings.production
+$manage compress --settings=ndohyep.settings.production
+$manage collectstatic --noinput --settings=ndohyep.settings.production
