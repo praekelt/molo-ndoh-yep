41a986783dc5973b29be12bea790c747c2479462
Milton Madanda <madandat@gmail.com>
2015-06-08 17:44:47 +0200

    update translations from latest molo scaffold

--- ndohyep/sideloader/postinstall.sh
+++ ndohyep/sideloader/postinstall.sh
@@ -2,5 +2,10 @@ cd "${INSTALLDIR}/${NAME}/ndohyep/"
 manage="${VENV}/bin/python ${INSTALLDIR}/${NAME}/ndohyep/manage.py"
 
 $manage migrate --settings=ndohyep.settings.production
+
+# process static files
 $manage compress --settings=ndohyep.settings.production
 $manage collectstatic --noinput --settings=ndohyep.settings.production
+
+# compile i18n strings
+$manage compilemessages --settings=ndohyep.settings.production
