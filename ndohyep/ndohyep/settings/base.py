223851b76f181e762685fcb0d87781d5cc941667
JeyabalaMurugan <jeyabalp@techaffinity.com>
2015-07-06 15:38:33 +0530

    OS module included

--- ndohyep/ndohyep/settings/base.py
+++ ndohyep/ndohyep/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 from os.path import abspath, dirname, join
 from django.conf import global_settings
 from django.utils.translation import ugettext_lazy as _
+import os
 
 # Absolute filesystem path to the Django project directory:
 PROJECT_ROOT = dirname(dirname(dirname(abspath(__file__))))
