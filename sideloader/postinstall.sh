f1ca8d45e33878bd586252b786337c2c02a1e075
Simon de Haan <simon@praekeltfoundation.org>
2015-07-30 14:02:52 +0200

    collectstatic before compressing

--- sideloader/postinstall.sh
+++ sideloader/postinstall.sh
@@ -4,8 +4,8 @@ export DJANGO_SETTINGS_MODULE=ndohyep.settings.production
 $manage migrate
 
 # process static files
-$manage compress
 $manage collectstatic --noinput
+$manage compress
 
 # compile i18n strings
 $manage compilemessages
