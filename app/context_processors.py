a598820d67ea2c010e0bef9aed5bc4dc872491b9
JeyabalaMurugan <jeyabalp@techaffinity.com>
2015-07-31 19:50:39 +0530

    Try, except block removed

--- app/context_processors.py
+++ app/context_processors.py
@@ -1,5 +1,4 @@
 from app.forms import EditProfileForm
-from app.models import UserProfile
 
 
 def get_profile_data(request):
@@ -7,17 +6,14 @@ def get_profile_data(request):
     alias = ''
     date_of_birth = ''
     if request.user.is_authenticated():
-        try:
-            user = request.user
-            profile = user.profile
-            username = user.username
-            if profile.alias:
-                alias = profile.alias
-            else:
-                alias = 'Anonymous'
-            date_of_birth = profile.date_of_birth
-        except UserProfile.DoesNotExist:
-            pass
+        user = request.user
+        profile = user.profile
+        username = user.username
+        if profile.alias:
+            alias = profile.alias
+        else:
+            alias = 'Anonymous'
+        date_of_birth = profile.date_of_birth
     edit_profile_form = EditProfileForm(
         initial={
             'username': username,
