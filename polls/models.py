26b62fd22b4724e5f2c7093393e730e2e7c9cacf
Milton Madanda <madandat@gmail.com>
2015-08-07 12:52:52 +0200

    use default title field for polls

--- polls/models.py
+++ polls/models.py
@@ -10,55 +10,14 @@ HomePage.subpage_types += ['polls.Question']
 
 
 class Question(Page):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    page = models.ForeignKey(
-        'wagtailcore.Page',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+',
-        help_text=_("Choose Page")
-    )
-
-    def __str__(self):
-        return self.question_text
-
-    search_fields = Page.search_fields + (
-        index.SearchField('pub_date'),
-        index.SearchField('question_text'),
-    )
-
-    content_panels = Page.content_panels + [
-        FieldPanel('page'),
-        FieldPanel('pub_date'),
-        FieldPanel('question_text'),
-    ]
-
     parent_page_types = [
         'core.HomePage', 'core.SectionPage', 'core.ArticlePage']
     subpage_types = ['polls.Choice']
 
 
 class Choice(Page):
-    question = models.ForeignKey(
-        Question,
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL
-    )
-    choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-    def __str__(self):
-        return self.choice_text
-
-    search_fields = Page.search_fields + (
-        index.SearchField('choice_text'),
-    )
-
-    content_panels = Page.content_panels + [
-        FieldPanel('question'),
-        FieldPanel('choice_text'),
+    promote_panels = Page.promote_panels + [
         FieldPanel('votes'),
     ]
