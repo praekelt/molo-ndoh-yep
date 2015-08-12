e868fc61ccd030752ca8dae072c10508cef67b26
Simon de Haan <simon@praekeltfoundation.org>
2015-08-12 13:38:56 +0200

    use the apps login, not wagtails

--- ndohyep/settings/base.py
+++ ndohyep/settings/base.py
@@ -188,7 +188,7 @@ TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
 
 # Wagtail settings
 
-LOGIN_URL = 'wagtailadmin_login'
+LOGIN_URL = 'auth_login'
 LOGIN_REDIRECT_URL = 'wagtailadmin_home'
 
 WAGTAIL_SITE_NAME = "NDOH Youth Platform"
