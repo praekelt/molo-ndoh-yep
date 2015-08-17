142edd16c6aeab449fedadb23cd46dda00182ec6
JeyabalaMurugan <jeyabalp@techaffinity.com>
2015-08-17 19:53:08 +0530

    Age range choice option removed

--- author_tags/migrations/0001_initial.py
+++ author_tags/migrations/0001_initial.py
@@ -15,7 +15,6 @@ class Migration(migrations.Migration):
             name='Authortags',
             fields=[
                 ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='wagtailcore.Page')),
-                ('age_range_tags', models.CharField(default=0, max_length=10, null=True, blank=True, choices=[(b'All', b'All'), (b'10-14', b'10-14'), (b'15-19', b'15-19'), (b'20-24', b'20-24'), (b'25+', b'25+')])),
             ],
             options={
                 'abstract': False,
--- author_tags/models.py
+++ author_tags/models.py
@@ -1,4 +1,3 @@
-from django.db import models
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
 
@@ -6,26 +5,11 @@ from molo.core.models import ArticlePage
 
 ArticlePage.subpage_types += ['author_tags.Authortags']
 
-AGE_RANGE_CHOICES = (
-    ('All', 'All'),
-    ('10-14', '10-14'),
-    ('15-19', '15-19'),
-    ('20-24', '20-24'),
-    ('25+', '25+'),
-)
-
 
 class Authortags(Page):
     parent_page_types = ['core.ArticlePage']
-    age_range_tags = models.CharField(
-        max_length=10,
-        choices=AGE_RANGE_CHOICES,
-        default=0,
-        null=True,
-        blank=True
-    )
+
 
 Authortags.content_panels = [
     FieldPanel('title', classname='full title'),
-    FieldPanel('age_range_tags')
 ]
