72a73eea5b0518b52862db6ef071f10fcc1fa6aa
Saeed Marzban <Saeed.mb8@gmail.com>
2015-10-09 15:40:38 +0200

    added views to ndohyep

---
+++ ndohyep/views.py
@@ -0,0 +1,67 @@
+from django.views.generic.base import TemplateView
+from django.conf import settings
+
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
+
+from molo.core.models import ArticlePage
+from wagtail.wagtailsearch.models import Query
+
+import django_comments
+
+from molo.commenting.forms import MoloCommentForm
+from molo.commenting.models import MoloComment
+
+
+class CommentReplyForm(TemplateView):
+    form_class = MoloCommentForm
+    template_name = 'comments/reply.html'
+
+    def get(self, request, parent_id):
+        comment = get_object_or_404(
+            django_comments.get_model(), pk=parent_id,
+            site__pk=settings.SITE_ID)
+        form = MoloCommentForm(comment.content_object, initial={
+            'content_type': '%s.%s' % (
+                comment.content_type.app_label,
+                comment.content_type.model),
+            'object_pk': comment.object_pk,
+        })
+        return self.render_to_response({
+            'form': form,
+            'comment': comment,
+        })
+
+
+def search(request, results_per_page=10):
+    search_query = request.GET.get('q', None)
+    page = request.GET.get('p', 1)
+
+    if search_query:
+        results = ArticlePage.objects.live().search(search_query)
+        Query.get(search_query).add_hit()
+    else:
+        results = ArticlePage.objects.none()
+
+    paginator = Paginator(results, results_per_page)
+    try:
+        search_results = paginator.page(page)
+    except PageNotAnInteger:
+        search_results = paginator.page(1)
+    except EmptyPage:
+        search_results = paginator.page(paginator.num_pages)
+
+    return render(request, 'search/search_results.html', {
+        'search_query': search_query,
+        'search_results': search_results,
+        'results': results,
+    })
+
+
+def report_response(request, comment_pk):
+    comment = MoloComment.objects.get(pk=comment_pk)
+
+    return render(request, 'comments/report_response.html', {
+        'article': comment.content_object,
+    })
