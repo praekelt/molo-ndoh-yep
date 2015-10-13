d7aa947576a53547832f249ad2ea12f2951c82ea
Saeed Marzban <Saeed.mb8@gmail.com>
2015-10-13 07:31:51 +0200

    added tests for ndohyep

---
+++ ndohyep/tests/test_views.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from django.test.client import Client
+
+from molo.core.models import ArticlePage
+from molo.commenting.models import MoloComment
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.sites.models import Site
+
+
+class ViewsTestCase(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='tester',
+            email='tester@example.com',
+            password='tester')
+
+    def test_default_dob_in_registration_done(self):
+        client = Client()
+        client.login(username='tester', password='tester')
+        response = client.get(reverse('registration_done'))
+
+        year_25_ago = datetime.today().year - 25
+        self.assertContains(
+            response,
+            '<option value="%(year)s" selected="selected">%(year)s</option>' %
+            {'year': year_25_ago})
+
+
+class TestReportResponse(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='tester',
+            email='tester@example.com',
+            password='tester')
+
+    def test_report_response(self):
+        client = Client()
+        article = ArticlePage.objects.create(
+            title='article 1', depth=1,
+            subtitle='article 1 subtitle',
+            slug='article-1', path=[1])
+        comment = MoloComment.objects.create(
+            content_object=article, object_pk=article.id,
+            content_type=ContentType.objects.get_for_model(article),
+            site=Site.objects.get_current(), user=self.user,
+            comment='comment 1', submit_date=datetime.now())
+        response = client.get(reverse('report_response',
+                                      args=(comment.id,)))
+        self.assertContains(
+            response,
+            "This comment has been reported."
+        )
