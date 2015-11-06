b27d3fc83d16087e15bd901687fb5037664d870f
Milton Madanda <madandat@gmail.com>
2015-11-06 09:14:36 +0200

    delete old user profile models

---
+++ app/migrations/0006_auto_delete_old_user_profile_model.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app', '0005_copy_existing_user_profiles_to_new_model'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='userprofile',
+            name='user',
+        ),
+        migrations.DeleteModel(
+            name='UserProfile',
+        ),
+    ]
